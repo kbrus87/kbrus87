a0f9db61d1535d87162c34587374b786ddcdeada
kbrus87 <65690504+kbrus87@users.noreply.github.com>
2020-09-26 18:48:25 -0300

    Now with instagram photos

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ const fetch = require('node-fetch');
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];;
 (async() => {
-    const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
+        const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
 
-    const getWeatherDate = async() => {
-        let res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=rosario&appid=45b2fa569efdc7a2702c344b9da9d776');
-        let data = await res.json()
-        let sky = await `${data.weather[0].main}  <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />`;
-        let temp = await `${(data.main.temp - 273.15).toFixed(2)} °C`;
+        const getWeatherDate = async() => {
+            let res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=rosario&appid=45b2fa569efdc7a2702c344b9da9d776');
+            let data = await res.json()
+            let sky = await `${data.weather[0].main}  <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />`;
+            let temp = await `${(data.main.temp - 273.15).toFixed(2)} °C`;
 
-        return { temp, sky }
-    }
-    const getPhotosFromInstagram = async() => {
-        const res = await fetch('https://www.instagram.com/bruno.j87')
-        const text = await res.text();
-        const INSTAGRAM_REGEXP = new RegExp(
-            /<script type="text\/javascript">window\._sharedData = (.*);<\/script>/
-        );
-        const json = JSON.parse(text.match(INSTAGRAM_REGEXP)[1]);
-        const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
-            0,
-            8
-        );
-
+            return { temp, sky }
+        }
+        const getPhotosFromInstagram = async() => {
+            const res = await fetch('https://www.instagram.com/bruno.j87')
+            const text = await res.text();
+            const INSTAGRAM_REGEXP = new RegExp(
+                /<script type="text\/javascript">window\._sharedData = (.*);<\/script>/
+            );
+            const json = JSON.parse(text.match(INSTAGRAM_REGEXP)[1]);
+            const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
+                0,
+                8
+            );
+            return edges.map(({ node }) => ({
+                permalink: `https://www.instagram.com/p/${node.shortcode}/`,
+                media_url: node.thumbnail_src,
+            }));
+        };
     }
     const generateInstagramHTML = ({ media_url, permalink }) => `
     <a href='${permalink}' target='_blank'>
@@ -38,12 +42,11 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 
     const { temp, sky } = await getWeatherDate();
 
-    const date = new Date();
-    newMarkdown = markdownTemplate
-        .replace('%date%', `${days[date.getDay()]} ${date.getDate()} of ${months[date.getMonth()]}`)
-        .replace('%SKY%', `${sky}`)
-        .replace('%TEMP%', `${temp}`)
-        .replace(LATEST_INSTAGRAM, latestInstagramPhotos);
+    const date = new Date(); newMarkdown = markdownTemplate
+    .replace('%date%', `${days[date.getDay()]} ${date.getDate()} of ${months[date.getMonth()]}`)
+    .replace('%SKY%', `${sky}`)
+    .replace('%TEMP%', `${temp}`)
+    .replace(LATEST_INSTAGRAM, latestInstagramPhotos);
 
     //writes readme with changes
     await fs.writeFile('./README.md', newMarkdown);
