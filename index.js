3a1fde4e2d68378b0342ba055172b929d50d3377
kbrus87 <65690504+kbrus87@users.noreply.github.com>
2020-09-26 14:31:56 -0300

    Add new script to update changes

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs').promises
+
+
+;
+(async() => {
+    const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
+
+
+
+    //writes readme with changes
+    await fs.writeFile('./README.md', markdownTemplate);
+})()
